02b7326c8105c717607b9b0a9209fb43a7944b0e
athomas227 <andrewthomas@pursuit.org>
2023-11-10 23:51:35 -0500

    added comments to js

--- main.js
+++ main.js
@@ -1,9 +1,13 @@
 function addSongInfo() {
+    // Selects the form
     const formElement = document.querySelector("#form");
+    // Selects the list for song info
     const songInfo = document.querySelector("ul");
 
+    // Listens for form submission
     formElement.addEventListener("submit", (event) => {
         event.preventDefault();
+        // Retrieves input values from the form
         const albumArt = formElement.newArtwork.value;
         const artist = formElement.newArtist.value;
         const album = formElement.newAlbum.value;
@@ -12,24 +16,31 @@ function addSongInfo() {
         const price = formElement.newPrice.value;
         const stockStatus = formElement.newStock.value;
 
+        // Clears the form after submission
         formElement.reset();
 
+        // Checks if info is provided
         if (albumArt && artist && album && genre && year) {
+            // Creates a new song entry with provided data
             const entry = createSongEntry(albumArt, artist, album, genre, year, price, stockStatus);
             songInfo.insertBefore(entry, songInfo.firstChild);
         } else {
+            // Displays an error message if required fields are incomplete
             const errorBox = document.createElement("div");
             errorBox.textContent = "Please enter a value in all required fields.";
             errorBox.classList.add("error-box");
             formElement.appendChild(errorBox);
             
+            // Removes the error message after 5 seconds
             setTimeout(() => {
                 errorBox.remove();
             }, 5000);
         }
     });
 
+    // Creates a new song entry with provided information
     function createSongEntry(albumArt, artist, album, genre, year, price, stockStatus) {
+        // Creates buttons for editing and deleting
         const editButton = document.createElement("button");
         editButton.textContent = "Edit";
         editButton.addEventListener("click", () => {
@@ -43,6 +54,8 @@ function addSongInfo() {
             entry.remove();
         });
 
+        // Creates a new list item (song entry)
+        // Populates it with image, details, and buttons
         const entry = document.createElement("li");
         const image = createAlbumArtworkImage(albumArt);
         const details = createSongDetails(artist, album, genre, year, price, stockStatus);
@@ -57,7 +70,9 @@ function addSongInfo() {
         return entry;
     }
 
+    // Creates an image element for the album artwork
     function createAlbumArtworkImage(albumArt, altText) {
+        // Creates an image element with provided source and alternative text
         const image = document.createElement("img");
         image.src = albumArt;
         image.alt = altText;
@@ -65,7 +80,10 @@ function addSongInfo() {
         return image;
     }
     
+    // Creates a div element containing song details
     function createSongDetails(artist, album, genre, year, price, stockStatus) {
+        // Creates paragraphs for different details (artist, album, genre, year, price, stock status)
+        // Populates the details div with these paragraphs
         const details = document.createElement("div");
         
         const artistParagraph = document.createElement("p");
@@ -95,7 +113,11 @@ function addSongInfo() {
         return details;
     }
 
+    // Edits an existing song entry
     function editSong(entry, albumArt, artist, album, genre, year, price, stockStatus) {
+        // Clears the entry for editing
+        // Creates input fields for each piece of song information
+        // Creates a 'save' button and defines its behavior
         entry.innerHTML = "";
 
         const albumArtInput = createInputField("text", "newArtwork", albumArt);
@@ -140,7 +162,9 @@ function addSongInfo() {
         entry.appendChild(saveButton);
     }
 
+    // Creates an input field element
     function createInputField(type, name, value) {
+        // Creates an input field of specified type with a given name and value
         const inputField = document.createElement("input");
         inputField.type = type;
         inputField.name = name;
@@ -149,4 +173,5 @@ function addSongInfo() {
     }
 }
 
+// Initiates the function when the page loads
 addSongInfo();
